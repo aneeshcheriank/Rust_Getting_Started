862954936caab8ab2ed221d73a7035b2447c0610
aneeshcheriank <aneeshcheriank@gmail.com>
2024-12-09 09:35:17 +0100

    - updated readme

---
+++ Basics/compund_datatype.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let numbers:[i32; 5] = [1, 2, 3, 4, 5];
+    println!("array of numbers are {:?}", numbers);
+}
+//need to use "" for print not ''
+//without :? inside {} (println!) error is "cannot format with the default formater"
---
+++ Basics/primitive_datatypes.rs
@@ -0,0 +1,15 @@
+//Primitive datatypes
+// int, float, bool, char
+
+//Integers
+//Rust has signed and unsigned intergers types of different sizes.
+//i8, i16--i128: singned integers
+//u8 - u128: unsigned integers
+
+fn main(){
+    let x: i32 = -42;
+    let y: u32 = 100;
+
+    println!("signed Integer: {}", x);
+    println!("unsinged Integer: {}", y);
+}
