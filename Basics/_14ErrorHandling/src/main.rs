26718c339b0a7e1ccbc78cf18959deae8eb9c473
aneeshcheriank <aneeshcheriank@gmail.com>
2024-12-17 12:42:24 +0100

    error handling - Result

--- Basics/_14ErrorHandling/src/main.rs
+++ Basics/_14ErrorHandling/src/main.rs
@@ -1,16 +1,35 @@
-fn main() {
-    let result = devide(10.0, 0.0);
+// example for Option
+// fn main() {
+//     let result = devide(10.0, 0.0);
 
+//     match result{
+//         Some(x) => println!("Result:{x}"),
+//         None => println!("Can't divide by Zero!"),
+//     }
+// }
+
+// fn devide(x:f32, y:f32)->Option<f32>{
+//     if y == 0.0{
+//         None
+//     } else {
+//         Some(x/y)
+//     }
+// }
+
+// Example for Result
+fn main(){
+    let result = divide(10.0, 0.0);
     match result{
-        Some(x) => println!("Result:{x}"),
-        None => println!("Can't divide by Zero!"),
+        Ok(result) => println!("Result: {}", result),
+        Err(err) => println!("Error: {}", err),
     }
 }
 
-fn devide(x:f32, y:f32)->Option<f32>{
+fn divide(x:f32, y:f32)-> Result<f32, String>{
     if y == 0.0{
-        None
-    } else {
-        Some(x/y)
+        Err("Can't divide by 0".to_string())
+    } else{
+        // need to wrap inside ok function
+        Ok(x/y)
     }
 }
